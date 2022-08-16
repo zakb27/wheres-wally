e05b73cffd607768cd94acd7c4ebf392f21a6ae7
zakb27 <zakbrook02@gmail.com>
2022-08-16 18:12:58 +0100

    wheres wally functioning

--- src/components/Render.js
+++ src/components/Render.js
@@ -1,13 +1,24 @@
 import React from "react";
-
+import './render.css'
 const Render = ({image}) =>{
 
-    const drop =()=>{
+    const drop =(event)=>{
+        let wally={x:1857,y:747}
+        event.preventDefault();
+        const { pageX: x, pageY: y } = event;
+
+        console.log(x);
+        if(Math.abs(wally.x-x)<=50 && Math.abs(wally.y-y)<=50){
+            alert('wally found')
+        }
+
+
+
 
     }
 
     return(
-        <div className="game_container">
+        <div className="game_container" onClick={drop}>
             <div className="game_image">
                 <img src={image} alt="Wally image"/>
             </div>
